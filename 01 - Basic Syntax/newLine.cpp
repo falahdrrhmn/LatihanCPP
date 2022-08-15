af3af7e2178e4f9aa5cba93230b0de578ab430c6
Muhammad Falah A <mfalah16@gmail.com>
2022-08-15 07:15:22 +0700

    newline

---
+++ 01 - Basic Syntax/newLine.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout<< "halo cantik \n";
+    cout<< "halo cantik" << endl;
+    cout<< "halo cantik new line lagi make endl";
+
+    return 0;
+}
