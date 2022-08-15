607c42a531e3175e5c0c5d3e956f798170e40c79
Muhammad Falah A <mfalah16@gmail.com>
2022-08-15 07:27:09 +0700

    Variable di c++

---
+++ 02 - Variables/variabeldiCPP.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    int usia = 10; 
+    string nama = "Muhammad Falah Abdurrohman";
+    double koma = 10.2;
+    char karakter = 'a';
+    bool pilihan = true;
+
+    cout << "kalo int hasilnya : " << usia << "\n";
+    cout << "kalo string hasilnya : " << nama << "\n";
+    cout << "kalo double hasilnya : " << koma << "\n";
+    cout << "kalo char hasilnya : " << karakter << "\n";
+    cout << "kalo bool hasilnya : " << pilihan << "\n";
+}
