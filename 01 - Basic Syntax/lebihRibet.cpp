1c6de2325e0cec7ac060ac3d9a3f57cf722c2309
Muhammad Falah A <mfalah16@gmail.com>
2022-08-15 07:06:53 +0700

    tambah file ygy

---
+++ 01 - Basic Syntax/lebihRibet.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!";
+    return 0;
+
+    // ini komen
+    /*
+    kalo ini 
+    multiline 
+    comments 
+    yang artinya 
+    gabakal dieksekusi:)
+    */
+}
