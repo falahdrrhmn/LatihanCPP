f38e5078b761b96fb24edde3952c739a1d4a0dda
Muhammad Falah A <mfalah16@gmail.com>
2022-08-19 10:10:33 +0700

    class method  belom lengkap:)

---
+++ 15 - OOP/02 - Class Methods/classMethods.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+// method merupakan function yang dimiliki oleh class
+// Ada dua cara untuk mendefinisikan fungsi yang termasuk dalam kelas:
+// 1. Inside class definition
+// 2. Outside class definition
