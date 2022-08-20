a2f19c1533bfa7ecd84da9fc017ef057d16be375
Muhammad Falah A <mfalah16@gmail.com>
2022-08-20 14:19:08 +0700

    access specifier praktik

--- 15 - OOP/04 - Access Specifier/accessSpecifier.cpp
+++ 15 - OOP/04 - Access Specifier/accessSpecifier.cpp
@@ -1,3 +1,19 @@
 #include <iostream>
 using namespace std;
 
+// contoh access specifier
+class MyClass{
+    public:    // Public access specifier
+        int x; // Public attribute
+    private:   // Private access specifier
+        int y; // Private attribute
+};
+
+int main(){
+    MyClass myObj;
+    myObj.x = 25; // Allowed (public)
+    myObj.y = 50; // Not allowed (private)
+    return 0;
+}
+
+// akan nampilin error karena y itu private
