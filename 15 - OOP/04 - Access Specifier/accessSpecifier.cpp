eac596c876c7cbaff6b7d341cc7dcb4d1ad986fd
Muhammad Falah A <mfalah16@gmail.com>
2022-08-20 14:19:32 +0700

    benerin dikit bagian praktik

--- 15 - OOP/04 - Access Specifier/accessSpecifier.cpp
+++ 15 - OOP/04 - Access Specifier/accessSpecifier.cpp
@@ -12,7 +12,7 @@ class MyClass{
 int main(){
     MyClass myObj;
     myObj.x = 25; // Allowed (public)
-    myObj.y = 50; // Not allowed (private)
+    // myObj.y = 50; // Not allowed (private)
     return 0;
 }
 
