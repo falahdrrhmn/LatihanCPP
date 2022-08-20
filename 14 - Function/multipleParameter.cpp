366fd7bb0c9878b5d32fc66f00fe63c909d2e990
Muhammad Falah A <mfalah16@gmail.com>
2022-08-20 14:02:23 +0700

    multiple parameter di function

---
+++ 14 - Function/multipleParameter.cpp
@@ -0,0 +1,16 @@
+# include <iostream>
+using namespace std;
+
+void functionKu(string nama, int usia){
+    cout<< "nama saya adalah " << nama << " dan usia saya adalah " << usia;
+}
+
+int main(){
+
+    functionKu("falah", 17);
+
+    return 0;
+}
+
+// output
+// nama saya adalah falah dan usia saya adalah 17
